4871648a8ac8069825f592460f150d4833da74cb
BATMAN <apprentice@dbc-chicago-34.local>
2014-02-12 15:06:05 -0600

    created table and student class

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,15 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+  def age
+    now = Date.today
+    age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+    age
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
 end
